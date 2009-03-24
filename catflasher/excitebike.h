0f87050ed9587f4d06b1cd0e8752d8b53be18011
mRB0 <mrburke@gmail.com>
2009-03-24 19:30:53 +0000

    add some docs, and Catalyst flash chip programmer
    --HG--
    extra : convert_revision : svn%3A8d6eca06-2316-de11-9fcd-000c29fc710c/trunk%4021

---
+++ catflasher/excitebike.h
@@ -0,0 +1,9 @@
+#ifndef EXCITEBIKE_H_
+#define EXCITEBIKE_H_
+
+#include "stdint.h"
+
+extern const rom unsigned char excitebike[];
+extern const rom uint16_t excitebike_len;
+
+#endif
---
+++ catflasher/stdint.h
@@ -0,0 +1,14 @@
+#ifndef __STDINT_H
+#define __STDINT_H
+
+typedef unsigned char uint8_t;
+typedef unsigned int uint16_t;
+typedef unsigned short long uint24_t;
+typedef unsigned long uint32_t;
+
+typedef signed char int8_t;
+typedef signed int int16_t;
+typedef signed short long uint24_t;
+typedef signed long int32_t;
+
+#endif
