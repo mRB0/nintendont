be53644364edda54815675a4983d20926a27629f
mukunda <none@none>
2009-03-29 07:33:53 +0000

    adjust vrc6 clock to 2.048 mhz
    --HG--
    extra : convert_revision : svn%3A8d6eca06-2316-de11-9fcd-000c29fc710c/trunk%4046

--- LUT/vrc6_ftab.py
+++ LUT/vrc6_ftab.py
@@ -5,10 +5,12 @@ output = open("vrc6_ftab.c", "w")
 output.write("#include \"stdint.h\"\n")
 output.write("const rom uint16_t vrc6_ftab[] = {")
 
+VRC6_CLK = 2048000.0
+
 for i in range(0,767):
     if( (i % 64) == 0 ):
         output.write( "\n    " )
-    output.write( str(int(round(6848.347156570608633265574554582 / 2**(i/768.0)))))
+    output.write( str(int(round( (VRC6_CLK/8363.0) * 2.0**5.0 / 2.0**(i/768.0) ))));
     output.write( ", " )
 
 output.write( "\n};\n" )
