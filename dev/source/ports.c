8d1642bfb7824dd64ce6edb07d638f772ac89ad9
mukunda <none@none>
2009-03-31 09:03:50 +0000

    some things
    --HG--
    extra : convert_revision : svn%3A8d6eca06-2316-de11-9fcd-000c29fc710c/trunk%4056

--- dev/source/ports.c
+++ dev/source/ports.c
@@ -11,53 +11,6 @@ uint8_t SPC_OPEN;
 uint8_t FLASH_OPEN;
 uint24_t FLASH_ADDR;
 
-/*
-uint8_t port_read(uint16_t addr) {
-	switch( addr ) {
-	case PORT_SPC0:
-		return SPCEMU_READPORT(0);
-	case PORT_SPC1:
-		return SPCEMU_READPORT(1);
-	case PORT_SPC2:
-		return SPCEMU_READPORT(2);
-	case PORT_SPC3:
-		return SPCEMU_READPORT(3);
-	}
-	return 0;
-}
-
-void port_write(uint16_t addr, uint8_t data) {
-	switch( addr ) {
-	case PORT_SPC0:
-		SPCEMU_WRITEPORT( 0, data ); break;
-	case PORT_SPC1:
-		SPCEMU_WRITEPORT( 1, data ); break;
-	case PORT_SPC2:
-		SPCEMU_WRITEPORT( 2, data ); break;
-	case PORT_SPC3:
-		SPCEMU_WRITEPORT( 3, data ); break;
-	case PORT_VRC690:
-		VRC6EMU_WRITEREG( 0, data ); break;
-	case PORT_VRC691:
-		VRC6EMU_WRITEREG( 1, data ); break;
-	case PORT_VRC692:
-		VRC6EMU_WRITEREG( 2, data ); break;
-	case PORT_VRC6A0:
-		VRC6EMU_WRITEREG( 3, data ); break;
-	case PORT_VRC6A1:
-		VRC6EMU_WRITEREG( 4, data ); break;
-	case PORT_VRC6A2:
-		VRC6EMU_WRITEREG( 5, data ); break;
-	case PORT_VRC6B0:
-		VRC6EMU_WRITEREG( 6, data ); break;
-	case PORT_VRC6B1:
-		VRC6EMU_WRITEREG( 7, data ); break;
-	case PORT_VRC6B2:
-		VRC6EMU_WRITEREG( 8, data ); break;
-	}
-}
-*/
-
 void ports_spc_open( void ) {
 	if( FLASH_OPEN )
 		printf( "warning: opening spc when flash is open\n" );
