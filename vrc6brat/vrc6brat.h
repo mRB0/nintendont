fe7ffc661498a92bcbda56e65b9bfbcf28ffa5d9
Mike Burke <mrburke@gmail.com>
2012-07-02 17:15:35 -0300

    vrc6brat: Numerous changes.
    * Move ports around to support physical build.
    * Add digital sample output with sample sample, currently from capslock.mod and plays repeatedly.

---
+++ vrc6brat/vrc6brat.h
@@ -0,0 +1,12 @@
+#ifndef VRC6BRAT_H
+#define VRC6BRAT_H
+
+#include <avr/pgmspace.h>
+
+extern uint8_t const VRC6AddrBase;
+extern uint8_t const VRC6AddrChanShift;
+extern uint8_t const VRC6Pulse1Base;
+extern uint8_t const VRC6Pulse2Base;
+extern uint8_t const VRC6SawBase;
+
+#endif
--- vrc6brat/vrc6brat.ino
+++ vrc6brat/vrc6brat.ino
@@ -3,47 +3,36 @@
 #include "simpleplayer.h"
 #include "ports.h"
 
-static int const pinD0 = 30;
-static int const pinD1 = 31;
-static int const pinD2 = 32;
-static int const pinD3 = 33;
-static int const pinD4 = 34;
-static int const pinD5 = 35;
-static int const pinD6 = 36;
-static int const pinD7 = 37;
+#include "vrc6brat.h"
 
-static int const pinA0 = 22;
-static int const pinA1 = 23;
-static int const pinA12 = 24;
-static int const pinA13 = 25;
-static int const pinA14 = 26;
+#define ADDR_PORT PORTA
+#define ADDR_PORT_DDR DDRA
 
-static int const pin_WE = 42;
-static int const pin_CE = 43;
+#define DATA_PORT PORTC
+#define DATA_PORT_DDR DDRC
 
-static int const pinVRC6OSCEN = 27;
+static uint8_t const pin_WE = 44;
+static uint8_t const pin_CE = 38;
 
-void writeAddr(uint16_t addr) {
-    digitalWrite(pinA0, (addr >> 0) & 0x01);
-    digitalWrite(pinA1, (addr >> 1) & 0x01);
-    digitalWrite(pinA12, (addr >> 12) & 0x01);
-    digitalWrite(pinA13, (addr >> 13) & 0x01);
-    digitalWrite(pinA14, (addr >> 14) & 0x01);
+uint8_t const VRC6AddrBase = 0x0; // 0x8000 on NES
+uint8_t const VRC6AddrChanShift = 2;
+uint8_t const VRC6Pulse1Base = VRC6AddrBase | (1 << VRC6AddrChanShift); // 0x9000 on NES
+uint8_t const VRC6Pulse2Base = VRC6AddrBase | (2 << VRC6AddrChanShift); // 0xa000 on NES
+uint8_t const VRC6SawBase = VRC6AddrBase | (3 << VRC6AddrChanShift); // 0xb000 on NES
+
+//static int const pinVRC6OSCEN = 27;
+
+void writeAddr(uint8_t addr) {
+    ADDR_PORT = addr;
 }
 
 void writeData(uint8_t value) {
-    digitalWrite(pinD0, (value >> 0) & 0x01);
-    digitalWrite(pinD1, (value >> 1) & 0x01);
-    digitalWrite(pinD2, (value >> 2) & 0x01);
-    digitalWrite(pinD3, (value >> 3) & 0x01);
-    digitalWrite(pinD4, (value >> 4) & 0x01);
-    digitalWrite(pinD5, (value >> 5) & 0x01);
-    digitalWrite(pinD6, (value >> 6) & 0x01);
-    digitalWrite(pinD7, (value >> 7) & 0x01);
+    DATA_PORT = value;
 }
 
-void port_write(uint16_t addr, uint8_t data) {
+void port_write(uint8_t addr, uint8_t data) {
     writeAddr(addr);
+    DATA_PORT_DDR = 0xff;
     writeData(data);
     digitalWrite(pin_WE, 1);
     digitalWrite(pin_CE, 0);
@@ -57,102 +46,49 @@ void port_write(uint16_t addr, uint8_t data) {
 }
 
 void setup() {
-    digitalWrite(pinVRC6OSCEN, 0);
-    pinMode(pinVRC6OSCEN, OUTPUT);
-    digitalWrite(pinVRC6OSCEN, 0);
+//    digitalWrite(pinVRC6OSCEN, 0);
+//    pinMode(pinVRC6OSCEN, OUTPUT);
+//    digitalWrite(pinVRC6OSCEN, 0);
     
     pinMode(pin_CE, OUTPUT);
     pinMode(pin_WE, OUTPUT);
     digitalWrite(pin_CE, 1);
     digitalWrite(pin_WE, 1);
-    
+
+    ADDR_PORT_DDR = 0xff;
     writeAddr(0);
     writeData(0);
     
-    pinMode(pinD0, OUTPUT);
-    pinMode(pinD1, OUTPUT);
-    pinMode(pinD2, OUTPUT);
-    pinMode(pinD3, OUTPUT);
-    pinMode(pinD4, OUTPUT);
-    pinMode(pinD5, OUTPUT);
-    pinMode(pinD6, OUTPUT);
-    pinMode(pinD7, OUTPUT);
-    pinMode(pinA0, OUTPUT);
-    pinMode(pinA1, OUTPUT);
-    pinMode(pinA12, OUTPUT);
-    pinMode(pinA13, OUTPUT);
-    pinMode(pinA14, OUTPUT);
-    pinMode(pin_CE, OUTPUT);
-    pinMode(pin_WE, OUTPUT);
-
     // init vrc6
     
-    port_write(0x9003, 0x00);
+    port_write(VRC6AddrBase | (0x1 << VRC6AddrChanShift) | 0x03, 0x00); // 0x9003
 
-	port_write(0x8000, 0x00);
-	port_write(0xc000, 0x00);
+	port_write(VRC6AddrBase, 0x00); // 0x8000
+	port_write(VRC6AddrBase | (0x4 << VRC6AddrChanShift), 0x00); // 0xc000
 
-	port_write(0xb003, 0x08);
-	port_write(0xf000, 0x00);
-	port_write(0xf001, 0x00);
-	port_write(0xf002, 0x00);
+	port_write(VRC6SawBase | 0x3, 0x08); // 0xa003
+	port_write(VRC6AddrBase | (0x7 << VRC6AddrChanShift) | 0x00, 0x00); // 0xf000
+	port_write(VRC6AddrBase | (0x7 << VRC6AddrChanShift) | 0x01, 0x00); // 0xf001
+	port_write(VRC6AddrBase | (0x7 << VRC6AddrChanShift) | 0x02, 0x00); // 0xf002
 
     // init vrc6 sound
 
-	port_write(0x9000, 0x7f);
-	port_write(0x9001, 0x00);
-	port_write(0x9002, 0x00);
+	port_write(VRC6Pulse1Base | 0x0, 0x7f);
+	port_write(VRC6Pulse1Base | 0x1, 0x00);
+	port_write(VRC6Pulse1Base | 0x2, 0x00);
 
-	port_write(0xa000, 0x7f);
-	port_write(0xa001, 0x00);
-	port_write(0xa002, 0x00);
+	port_write(VRC6Pulse2Base | 0x0, 0x7f);
+	port_write(VRC6Pulse2Base | 0x1, 0x00);
+	port_write(VRC6Pulse2Base | 0x2, 0x00);
 
-	port_write(0xb000, 0x3f);
-	port_write(0xb001, 0x00);
-	port_write(0xb002, 0x00);
+	port_write(VRC6SawBase | 0x0, 0x3f);
+	port_write(VRC6SawBase | 0x1, 0x00);
+	port_write(VRC6SawBase | 0x2, 0x00);
 
-    digitalWrite(pinVRC6OSCEN, 1);
+//    digitalWrite(pinVRC6OSCEN, 1);
 }
-
-void funplayer(void);
 
 void loop(void) {
     simpleplayer();
     //funplayer();
-}
-
-void funplayer(void) {
-    uint8_t duty = 0;
-    
-    for(;;) {
-        //duty = (duty + 1) % 0x8;
-        duty = 7;
-        
-        port_write(0x9000, 0x0f | (duty << 4));
-        //port_write(0xb000, 0x0f | (duty << 4));
-
-        // set pitches
-
-        //port_write(0xb001, 0xa7);
-        //port_write(0xb002, 0x83);
-        port_write(0x9001, 0xe7);
-        port_write(0x9002, 0x82);
-
-        delay(100);
-
-        //port_write(0xb001, 0xe7);
-        //port_write(0xb002, 0x82);
-        port_write(0x9001, 0x72);
-        port_write(0x9002, 0x82);
-
-        delay(100);
-
-        //port_write(0xb001, 0x72);
-        //port_write(0xb002, 0x82);
-        port_write(0x9001, 0xd6);
-        port_write(0x9002, 0x81);
-        
-        delay(100);
-
-    }
 }
